0a428d84ed5d2f09771ef33aa771c299ed586a02
Alex Goller <alpipego@protonmail.com>
2022-03-07 19:53:27 -0600

    Improve dashboard output.
    Minor tweaks, sanitize post_args before querying.
    Remove dangling `get_terms` call.

--- admin/views/dashboard.php
+++ admin/views/dashboard.php
@@ -12,78 +12,67 @@ if (!defined('WPINC')) {
 }
 
 /**
- * Get available markers.
- *
- * @since     1.0.0
- */
-$marker_args = [
-    'hide_empty' => true,
-];
-$markers = get_terms('marker', $marker_args);
-
-/**
  * Build marker stats for each post type.
  *
- * @since     1.0.8
+ * @since 1.0.8
  */
 function get_marker_stats()
 {
     // attempt to get the stats from the transient
-    if (false === ($latest_marker_stats = get_transient('marker_posts_stats'))) {
-        $marker_args = [
-            'hide_empty' => true,
-        ];
-        $markers = get_terms('marker', $marker_args);
-        $get_mark_posts_setup = get_option('mark_posts_settings');
-        $mark_posts_posttypes = $get_mark_posts_setup['mark_posts_posttypes'];
-        $marker_stats = '';
-
-        foreach ($mark_posts_posttypes as $mark_posts_posttype) {
-            $marked_posts = '';
-
-            foreach ($markers as $marker) {
-                $post_args = [
-                    'post_type'      => $mark_posts_posttype,
-                    'taxonomy'       => $marker->taxonomy,
-                    'term'           => $marker->slug,
-                    'post_status'    => ['publish', 'pending', 'draft', 'future'],
-                    'posts_per_page' => -1,
-                ];
-                $posts = get_posts(apply_filters('mark_posts_dashboard_query', $post_args));
-                $posts_count = count($posts);
-
-                if (!empty($posts_count)) {
-                    $marked_posts .= '<li class="mark-posts-info mark-posts-'.$marker->slug.'">';
-                    $marked_posts .= '<a a href="edit.php?post_type='.$mark_posts_posttype.'&marker='.$marker->slug.'">'.$posts_count.' '.$marker->name.'</a>';
-                    $marked_posts .= '</li>';
-                }
-            } // end of marker loop
-
-            $marker_post_type_object = get_post_type_object($mark_posts_posttype);
-
-            if (!empty($marked_posts)) {
-                $marker_stats .= '<h3 class="mark_posts_headline">'.$marker_post_type_object->labels->name.'</h3>';
-                $marker_stats .= '<ul class="markers_right_now">';
-                $marker_stats .= $marked_posts;
-                $marker_stats .= '</ul>';
-            }
-        } // end of post type loop
-
-        // set transient
-        set_transient('marker_posts_stats', $marker_stats, 60 * 60 * 12);
-
-        if (!empty($marker_stats)) {
-            return $marker_stats;
-        } else {
-            return __('No marked posts yet.', 'mark-posts');
-        }
-    } else {
-        return get_transient('marker_posts_stats');
+    $transient_data = get_transient('marker_posts_stats');
+    if ($transient_data) {
+        return $transient_data;
     }
+
+    $markers              = get_terms(['taxonomy' => 'marker']);
+    $mark_posts_posttypes = get_option('mark_posts_settings')['mark_posts_posttypes'];
+    $marker_stats         = '';
+
+    foreach ($mark_posts_posttypes as $mark_posts_posttype) {
+        $marked_posts = '';
+
+        foreach ($markers as $marker) {
+            $default_args = [
+                'post_type'      => $mark_posts_posttype,
+                'taxonomy'       => $marker->taxonomy,
+                'term'           => $marker->slug,
+                'post_status'    => ['publish', 'pending', 'draft', 'future'],
+                'posts_per_page' => -1,
+            ];
+            $post_args    = apply_filters('mark_posts_dashboard_query', $default_args);
+            if (!is_array($post_args)) {
+                $post_args = $default_args;
+            }
+            $posts_count = (new WP_Query($post_args))->post_count;
+
+            if (!empty($posts_count)) {
+                $marked_posts .= '<li class="mark-posts-info mark-posts-' . $marker->slug . '">';
+                $marked_posts .= '<a href="edit.php?post_type=' . $mark_posts_posttype . '&marker=' . $marker->slug . '">' . $posts_count . ' ' . $marker->name . '</a>';
+                $marked_posts .= '</li>';
+            }
+        } // end of marker loop
+
+        $marker_post_type_object = get_post_type_object($mark_posts_posttype);
+        if ($marker_post_type_object === null) {
+            continue;
+        }
+
+        if (!empty($marked_posts)) {
+            $marker_stats .= '<h3 class="mark_posts_headline">' . $marker_post_type_object->labels->name . '</h3>';
+            $marker_stats .= '<ul class="markers_right_now">';
+            $marker_stats .= $marked_posts;
+            $marker_stats .= '</ul>';
+        }
+    } // end of post type loop
+
+    // set transient
+    set_transient('marker_posts_stats', $marker_stats, 60 * 60 * 12);
+
+    if (!empty($marker_stats)) {
+        return $marker_stats;
+    }
+
+    return __('No marked posts yet.', 'mark-posts');
 }
 
-if (!empty($markers)) {
-    echo get_marker_stats();
-} else {
-    _e('No marked posts yet.', 'mark-posts');
-}
+echo get_marker_stats();
